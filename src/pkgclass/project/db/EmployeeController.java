b0ad5a4a132d69ef6f7633814e78813f86df6ec6
azizg <azizg@DESKTOP-R6MUB3S.mshome.net>
2020-06-03 21:13:59 +0200

    intial commit

---
+++ src/pkgclass/project/db/EmployeeController.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkgclass.project.db;
+
+import java.sql.CallableStatement;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.table.DefaultTableModel;
+
+public class EmployeeController  extends DBConnection {
+    
+    public EmployeeController() {
+        connnectionMethod();
+    }
+    //works
+    public void create(String name,String email, int factoryId) throws SQLException{
+        String query = "{CALL insert_employee('"+name+"','"+email+"','"+factoryId+"')}";
+        CallableStatement stmt = connection.prepareCall(query);
+        stmt.execute();
+    }
+    //works
+    public void select(DefaultTableModel dtm) throws SQLException{
+       String sql = "SELECT * FROM employee";
+       resultSet = statement.executeQuery(sql);
+       Object[] data= new Object[4];
+       while(resultSet.next()){
+           data[0] = resultSet.getInt("employee_id");
+           data[1] = resultSet.getString("factory_id");
+           data[2] = resultSet.getString("name");
+           data[3] = resultSet.getString("email");
+           dtm.addRow(data);
+       }
+    }
+    //works
+    public void update(int id, String name, String email) throws SQLException{
+        String query = "{CALL update_employee('"+name+"','"+email+"','"+id+"')}";
+        CallableStatement stmt = connection.prepareCall(query);
+        stmt.executeUpdate();
+    }
+    //works
+    public void delete(int id ) throws SQLException{
+        String query = "{CALL delete_from_employees('"+id+"')}";
+        CallableStatement stmt = connection.prepareCall(query);
+        stmt.executeUpdate();
+    }
+}
